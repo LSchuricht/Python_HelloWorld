be36e03ab80d3434e5d9470e84a9c8d19d1db475
Lucas Schuricht <lucas.schuricht@fielmann.com>
2023-10-29 14:51:34 +0100

    feat: lint sissor_paper_rock

--- python/sissor_paper_rock/main.py
+++ python/sissor_paper_rock/main.py
@@ -1,24 +1,29 @@
-import random
+from random import choice
 
 
 def dice():
-    com = random.choice(["Sissor","Paper","Rock"])
+    com = choice(["Sissor", "Paper", "Rock"])
     user_input = input("Please Write Sissor:\n")
 
-    if user_input == "Sissor" and com == "Sissor": printmessage("draw")
-    elif user_input == "Sissor" and com == "Paper": printmessage("win")
-    elif user_input == "Sissor" and com == "Rock": printmessage("lost")
-    else: printmessage("you wrote sth wrong")
+    if user_input == "Sissor" and com == "Sissor":
+        printmessage("draw")
+    elif user_input == "Sissor" and com == "Paper":
+        printmessage("win")
+    elif user_input == "Sissor" and com == "Rock":
+        printmessage("lost")
+    else:
+        printmessage("you wrote sth wrong")
+
 
 def printmessage(msg):
     if msg == "draw":
         print("its a draw")
     elif msg == "win":
         print("its a win")
-    elif msg == "lost": 
+    elif msg == "lost":
         print("it a lost")
-    else: print(msg)    
-
+    else:
+        print(msg)
 
 
 dice()
