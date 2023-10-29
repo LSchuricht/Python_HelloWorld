bed20fab547be68f8cb780e0694cca4658ce2ffd
Lucas Schuricht <lucas.schuricht@fielmann.com>
2023-10-29 14:29:05 +0100

    feat: testcase 7

--- python/add_two_numbers/testcases.py
+++ python/add_two_numbers/testcases.py
@@ -32,3 +32,8 @@ class test_basic(TestCase):
         mycase = add_two_numbers([], [])
         mycase.foo()
         self.assertEqual(mycase.l3, ["0"])
+
+    def test_case_7(self):
+        mycase = add_two_numbers([2], [3, 9, 8, 5, 2, 3, 0, 6])
+        mycase.foo()
+        self.assertEqual(mycase.l3, ["8", "0", "3", "2", "5", "8", "9", "3"])
